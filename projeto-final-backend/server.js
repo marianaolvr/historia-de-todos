cf0785b65cffd760f3f1e272673f8a7f823b9247
Mariana Oliveira <olvr.mariana@gmail.com>
2019-07-10 21:53:22 -0300

    Chama o body-parser e o cors

--- projeto-final-backend/server.js
+++ projeto-final-backend/server.js
@@ -1,23 +1,32 @@
 const express = require('express')
 const server = express()
-const controller = require('./ReclamacoesController') //é maiusculo?
+const controller = require('./reclamacoesController')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const PORT = 3000
 
-server.get('/', (request, response) => {
-    response.send('Olá, mundo!')
-  })
+server.use(cors())
+server.use(bodyParser.json())
 
-server.post("/reclamacoes", async (request, response) =>{
-    response.send()
-    .then
+  server.get("/reclamacoes", async (request, response) => {
+    controller.getAll()
+  .then(listaDeReclamacoes => response.send(listaDeReclamacoes))
 })
 
-server.get("/reclamacoes/enviar", async (request, response) => {
-    response.send(200)
-    .then
+server.post("/reclamacoes/enviar", async (request, response) =>{
+    controller.add(request.body)
+    .then(reclamacao => {
+      const _id = reclamacao._id
+      response.send(_id)
+    })
+    .catch(error => {
+      if(error.name === "ValidationError"){
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
 })
-
-
 
 server.listen(PORT)
 console.info(`Rodando na porta ${PORT}`)
